deab0b9ce7d8276bfc76f238f639170ff2946d0b
Dragos <dragos.giugula@gmail.com>
2018-08-12 15:26:23 +0300

    Updated Nav Bar

--- src/App/Layouts/App.jsx
+++ src/App/Layouts/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../Features /event/eventDashboard/EventDashboard";
+import NavBar from "../../Features /event/Nav/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Reevent </h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
