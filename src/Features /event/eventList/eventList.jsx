31d301a9a1b1c63b4aeb722cbdb1046cbca8315d
Dragos <dragos.giugula@gmail.com>
2018-08-13 10:56:27 +0300

    New Changes

--- src/Features /event/eventList/eventList.jsx
+++ src/Features /event/eventList/eventList.jsx
@@ -3,13 +3,13 @@ import EventListItem from "./eventListItem";
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <div>
-        <h1> Event List </h1>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
-        <h1> </h1>
+        <h1>Events List </h1>
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
       </div>
     );
   }
