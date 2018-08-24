14f3d6bf0a19ec71b35b9453cd39e063576d1112
Dragos <dragos.giugula@gmail.com>
2018-08-24 19:06:56 +0300

    Home Page Upate

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import { Menu, Container, Button } from 'semantic-ui-react';
-
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
 class NavBar extends Component {
   render() {
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={Link} to="/" header>
             <img src="assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
-          <Menu.Item name="Events" />
-          <Menu.Item>
-            <Button floated="right" positive inverted content="Create Event" />
-          </Menu.Item>
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
           <Menu.Item position="right">
-            <Button basic inverted content="Login" />
+            <Button
+              as={Link}
+              to="/createdEvent"
+              basic
+              inverted
+              content="Login"
+            />
             <Button
               basic
               inverted
               content="Sign Out"
-              style={{ marginLeft: '0.5em' }}
+              style={{ marginLeft: "0.5em" }}
             />
           </Menu.Item>
         </Container>
