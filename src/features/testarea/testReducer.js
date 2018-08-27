e942a790e17c3dd5d4fc0244ef6219f836734eed
Dragos <dragos.giugula@gmail.com>
2018-08-27 23:02:45 +0300

    fixed speeling error in testComponent

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -9,7 +9,7 @@ const testReducer = (state = initialState, action) => {
     case INCREMENT_COUNTER:
       return { ...state, data: state.data + 1 };
     case DECREMENT_COUNTER:
-      return { ...state, data: state.dta - 1 };
+      return { ...state, data: state.data - 1 };
     default:
       return state;
   }
