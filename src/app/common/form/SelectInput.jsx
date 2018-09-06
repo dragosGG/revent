40b77cff4980b471d5e01175e6ec391c952ad43b
Dragos <dragos.giugula@gmail.com>
2018-09-06 13:48:10 +0300

    For Redux Created

--- src/app/common/form/SelectInput.jsx
+++ src/app/common/form/SelectInput.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Form, Label } from "semantic-ui-react";
 
-const SelectInput = () => {
-  return <div />;
+const SelectInput = ({
+  input,
+  type,
+  placeholder,
+  multiple,
+  options,
+  meta: { touched, error }
+}) => {
+  return (
+    <Form.Field error={touched && !!error}>
+      <Select
+        value={input.value || null}
+        onChange={(e, data) => input.onChange(data.value)}
+        placeholder={placeholder}
+        options={options}
+        multiple={multiple}
+      />
+      {touched &&
+        error && (
+          <Label basic coloe="red">
+            {error}
+          </Label>
+        )}
+    </Form.Field>
+  );
 };
 
 export default SelectInput;
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -6,6 +6,7 @@ import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
 import { createEvent, updateEvent } from "../eventActions";
 import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
+import SelectInput from "../../../app/common/form/SelectInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -32,6 +33,14 @@ const actions = {
   updateEvent
 };
 
+const category = [
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "culture", text: "Culture", value: "culture" },
+  { key: "film", text: "Film", value: "film" },
+  { key: "food", text: "Food", value: "food" },
+  { key: "music", text: "Music", value: "music" },
+  { key: "travel", text: "Travel", value: "travel" }
+];
 class EventForm extends Component {
   onFormSubmit = evt => {
     evt.preventDefault();
