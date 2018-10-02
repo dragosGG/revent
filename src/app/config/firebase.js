d6316443b9c8dd009f60086ed8770d9aff95bd80
Dragos <dragos.giugula@gmail.com>
2018-10-02 16:59:15 +0200

    new additions

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,21 +1,21 @@
+
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-const firebaseConfig= {
-  apiKey: "AIzaSyBEoxbU7eoNb5ShfeOt6uCo_rlusqKIJGo",
-  authDomain: "modular-seeker-215717.firebaseapp.com",
-  databaseURL: "https://modular-seeker-215717.firebaseio.com",
-  projectId: "modular-seeker-215717",
-  storageBucket: "modular-seeker-215717.appspot.com",
-  messagingSenderId: "745343757670"
+const firebaseConfig={
+    apiKey: "AIzaSyBEoxbU7eoNb5ShfeOt6uCo_rlusqKIJGo",
+    authDomain: "modular-seeker-215717.firebaseapp.com",
+    databaseURL: "https://modular-seeker-215717.firebaseio.com",
+    projectId: "modular-seeker-215717",
+    storageBucket: "modular-seeker-215717.appspot.com",
+    messagingSenderId: "745343757670"
 }
 
 
-firebase.initializeApp(firebaseConfig);
-const firestore = firebase.firestore();
+firebase.initializeApp(firebaseConfig)
+const firebase = firebase.firestore();
 const settings = {
-  timestampsInSnapshots: true
-}
-firestore.settings(settings)
-export default firebase;
-
+    timestampsinsnapshots: true
+} 
+firestore.settings(settings);
+export default firebase 
